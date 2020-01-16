a4d537dbde8981587a09bf62e3fd3dc797328cd2
Qiugu He <kaochi545121151@gmail.com>
2020-01-16 14:14:04 -0600

    Add project files.

---
+++ IssueTrackerV1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IssueTrackerV1.Startup))]
+namespace IssueTrackerV1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
