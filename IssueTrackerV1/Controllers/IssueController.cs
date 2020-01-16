ee5a2dbd98dac30ddf61f36aff1ea7870489498e
Qiugu He <kaochi545121151@gmail.com>
2020-01-16 14:37:39 -0600

    Initi Repos; Init Issue MVC

---
+++ IssueTrackerV1/Controllers/IssueController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IssueTrackerV1.Models;
+
+namespace IssueTrackerV1.Controllers
+{
+    public class IssueController : Controller
+    {
+        // GET: Issue
+        public ActionResult Random()
+        {
+            var issue = new Issue() {Name = "ToDo1"};
+
+            return View(issue);
+        }
+    }
+}
---
+++ IssueTrackerV1/Models/Issue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IssueTrackerV1.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
