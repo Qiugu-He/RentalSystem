8c54a62838c8c558d316d8a3331dede3aa54a414
Qiugu He <kaochi545121151@gmail.com>
2020-01-17 16:08:53 -0600

    init memebership model

--- IssueTrackerV1/Models/MembershipType.cs
+++ IssueTrackerV1/Models/MembershipType.cs
@@ -7,6 +7,9 @@ namespace IssueTrackerV1.Models
 {
     public class MembershipType
     {
-
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
     }
 }
--- IssueTrackerV1/Models/User.cs
+++ IssueTrackerV1/Models/User.cs
@@ -10,5 +10,8 @@ namespace IssueTrackerV1.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+
     }
 }
