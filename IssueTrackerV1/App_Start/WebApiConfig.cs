7686c534bc20a44a2dc56074acbc8fffe13a7689
Qiugu He <kaochi545121151@gmail.com>
2020-01-27 20:42:11 -0600

    Camel Casing for JS

--- IssueTrackerV1/App_Start/WebApiConfig.cs
+++ IssueTrackerV1/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace IssueTrackerV1
 {
@@ -9,6 +11,11 @@ namespace IssueTrackerV1
     {
         public static void Register(HttpConfiguration config)
         {
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
